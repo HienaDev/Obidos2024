95ae86adc338704e91cc8a12ede85c27779b9d8c
notCroptu <martinsmariana1972@gmail.com>
2024-04-30 13:24:14 +0100

    Change Bird ScriptableObjects

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,5 +7,13 @@ public class Bird : ScriptableObject
     // sprite
     public Sprite sprite;
     // description
+    public string especie;
     public string description;
+    public string[] habits;
+    public float altura;
+
+    // crime
+    public string crime;
+    // crime description
+    public string crimeDescription;
 }
